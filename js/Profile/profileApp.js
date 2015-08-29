c42e65afb89943399ef85c83ac55c826fd9fe7a7
JonathanSpalding <Jon.Spalding@hotmail.com>
2015-08-29 09:48:55 -0600

    changed contenteditable boxes to input fields.

--- js/Profile/profileApp.js
+++ js/Profile/profileApp.js
@@ -1,9 +1,1 @@
-$(document).ready(function() {
-
-    $("#newGame").click(function() {
-      debugger
-        var domElement = $('<p contenteditable="true">Type Location Here</p>');
-        $(this).after(domElement);
-    });
-    
-});
+var app = angular.module('frolfZone');
